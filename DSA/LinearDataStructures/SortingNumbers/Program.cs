a617a96d44bc7a6e270d4916e7371c26767f1229
Vladislav Naydenov <ferry240@gmail.com>
2013-06-05 22:35:50 +0300

    Third problem.

---
+++ DSA/LinearDataStructures/SortingNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sequence of integers.");
+            string sequence = Console.ReadLine();
+            string[] integers = sequence.Split(' ');
+
+            var intList = integers.Select(integer => int.Parse(integer)).ToList();
+
+            intList.Sort();
+
+            foreach (var i in intList)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
