77edea236d129eb17bb87d7a9967a9aab8d79648
Vladislav Naydenov <ferry240@gmail.com>
2013-06-05 22:34:56 +0300

    Second problem.

---
+++ DSA/LinearDataStructures/2. PushInTheStack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.PushInTheStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var integers = new Stack<int>();
+
+            for (string empty = null; (empty = Console.ReadLine()) != string.Empty; )
+                integers.Push(int.Parse(empty));
+
+            Console.WriteLine(string.Join(" ", integers));
+        }
+    }
+}
