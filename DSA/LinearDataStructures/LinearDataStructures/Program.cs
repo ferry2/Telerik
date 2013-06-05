8829ee5fc272134e16d52a4e8098c446fcec9509
Vladislav Naydenov <ferry240@gmail.com>
2013-06-05 21:32:58 +0300

    Solved first problem.

--- DSA/LinearDataStructures/LinearDataStructures/Program.cs
+++ DSA/LinearDataStructures/LinearDataStructures/Program.cs
@@ -10,6 +10,15 @@ namespace LinearDataStructures
     {
         static void Main(string[] args)
         {
+            var integers = new List<int>();
+
+            for (string empty = null; (empty = Console.ReadLine()) != string.Empty;)
+            {
+                integers.Add(int.Parse(empty));
+            }
+
+            Console.WriteLine("\nThe Sum of the elments is {0} \n", integers.Sum());
+            Console.WriteLine("\nThe Average of the elments is {0} \n", integers.Average());
         }
     }
 }
