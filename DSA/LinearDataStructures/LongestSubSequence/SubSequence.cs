240fa82a230961be53f41270f805e2d1ad902561
Vladislav Naydenov <ferry240@gmail.com>
2013-06-07 22:16:50 +0300

    Solved problem 4. Added tests.

---
+++ DSA/LinearDataStructures/LongestSubSequence/SubSequence.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestSubSequence
+{
+    public class SubSequence
+    {
+        static void Main(string[] args)
+        {
+            var list = ReadIntegers();
+            var result = FindLongestSubsequence(list);
+            Print(result);
+        }
+
+        public static List<int> ReadIntegers()
+        {
+            int n;
+            string empty;
+
+            List<int> numbers = new List<int>();
+
+            do
+            {
+                Console.WriteLine("Enter numbers: ");
+                empty = Console.ReadLine();
+
+                if (int.TryParse(empty, out n))
+                {
+                    numbers.Add(n);
+                }
+
+            } while (empty != string.Empty);
+
+            return numbers;
+        }
+
+        public static List<int> FindLongestSubsequence(List<int> sequence)
+        {
+            if (sequence == null)
+            {
+                throw new ArgumentException("List is null.");
+            }
+
+            if (sequence.Count == 0)
+            {
+                throw new ArgumentException("The list does not contain elements.");
+            }            
+
+            int number = 0;
+            int sumMax = 1;
+            int equalsMax = 0;
+
+            for (int i = 0; i < sequence.Count - 1; i++)
+            {
+                if (sequence[i] == sequence[i + 1])
+                {
+                    sumMax += 1;
+                }
+                else
+                {
+                    equalsMax = 1;
+                }
+                
+                if (sumMax > equalsMax)
+                {
+                    equalsMax = sumMax;
+                    number = sequence[i];
+                }
+            }
+
+            List<int> resultList = new List<int>(equalsMax);
+            for (int i = 0; i < equalsMax; i++)
+            {
+                resultList.Add(number);
+            }
+
+            return resultList;
+        }
+
+        public static void Print(List<int> list)
+        {
+            Console.WriteLine(string.Join(",", list));          
+        }
+    }
+}
---
+++ DSA/LinearDataStructures/LongestSubSequenceTest/LongestSubSequenceTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LongestSubSequence;
+
+namespace LongestSubSequenceTest
+{
+    [TestClass]
+    public class LongestSubSequenceTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestEmptyList()
+        {
+            List<int> testList = new List<int>();
+
+            List<int> longestSub = SubSequence.FindLongestSubsequence(testList);
+            List<int> resultList = new List<int>();
+
+            Assert.AreEqual(resultList, longestSub);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestZeroElementsList()
+        {
+            List<int> testList = new List<int>(){};
+            List<int> longestSub = SubSequence.FindLongestSubsequence(testList);
+            List<int> resultList = new List<int>(){};
+
+            Assert.AreEqual(resultList, longestSub);
+        }
+
+        [TestMethod]       
+        public void TestOneElementsList()
+        {
+            List<int> testList = new List<int>() { 1 };
+            List<int> longestSub = SubSequence.FindLongestSubsequence(testList);
+            List<int> resultList = new List<int>() {1};
+
+            for (int i = 0; i < longestSub.Count - 1; i++)
+            {
+                Assert.AreEqual(resultList[i], longestSub[i]);
+            }                        
+        }
+
+        [TestMethod]
+        public void TestTwoElementsList()
+        {
+            List<int> testList = new List<int>() { 1, 5 };
+            List<int> longestSub = SubSequence.FindLongestSubsequence(testList);
+            List<int> resultList = new List<int>() { 5 };
+
+            for (int i = 0; i < longestSub.Count - 1; i++)
+            {
+                Assert.AreEqual(resultList[i], longestSub[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestManyElementsList()
+        {
+            List<int> testList = new List<int>()
+                {
+                    1, 5, 5, 5, 6, 3, 4, 56, 1, 5, 4, 4, 4, 4
+                };
+            List<int> longestSub = SubSequence.FindLongestSubsequence(testList);
+            List<int> resultList = new List<int>() { 4, 4, 4, 4, 4 };
+
+            for (int i = 0; i < longestSub.Count - 1; i++)
+            {
+                Assert.AreEqual(resultList[i], longestSub[i]);
+            }
+        }
+    }
+}
