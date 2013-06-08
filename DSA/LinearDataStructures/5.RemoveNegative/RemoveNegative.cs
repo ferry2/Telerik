9a6ee9a82971a2f23520e75e270e23f689c5cbc5
Vladislav Naydenov <ferry240@gmail.com>
2013-06-08 21:52:15 +0300

    Solved problem 5.

---
+++ DSA/LinearDataStructures/5.RemoveNegative/RemoveNegative.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.RemoveNegative
+{
+    public class RemoveNegative
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Console.WriteLine("Enter numbers: ");
+            string number = null;
+          
+            do
+            {
+                number = Console.ReadLine();
+
+                if (number != string.Empty)
+                {
+                    list.Add(int.Parse(number));    
+                }                
+            } while (number != string.Empty);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] < 0)
+                {
+                    list.Remove(list[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(",", list));
+        }
+    }
+}
