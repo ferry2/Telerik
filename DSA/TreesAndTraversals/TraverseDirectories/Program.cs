716c063f1c1490c97234f011bab0b875a078cb49
Vladislav Naydenov <ferry240@gmail.com>
2013-06-13 22:49:21 +0300

    Solved problem 2.

--- DSA/TreesAndTraversals/TraverseDirectories/Program.cs
+++ DSA/TreesAndTraversals/TraverseDirectories/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,56 @@ namespace TraverseDirectories
 
             List<string> dirs = new List<string>();
             List<string> exes = new List<string>();
+
+            StringBuilder errors = new StringBuilder();
+
+            Queue<string> rootDirs = new Queue<string>();
+
+            rootDirs.Enqueue(ROOT);
+
+            while (rootDirs.Count > 0)
+            {
+                var current = rootDirs.Peek();
+
+                try
+                {
+                    foreach (var dir in Directory.GetDirectories(current))
+                    {
+                        rootDirs.Enqueue(dir);
+                        dirs.Add(dir);
+                    }
+
+                    var files = Directory.GetFiles(current, "*.exe");
+                    exes.AddRange(files);
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    errors.AppendLine(ex.Message);
+                }
+
+                rootDirs.Dequeue();
+            }
+
+            if (errors.Length > 0)
+            {
+                Console.WriteLine("Errors: ");
+                Console.WriteLine(errors);
+            }
+
+            Console.WriteLine("Directories tree of C:\\Windows partition: ");
+            Print(exes);
+        }
+
+        private static void Print(IEnumerable<string> element)
+        {
+            StringBuilder elements = new StringBuilder();
+
+            foreach (var item in element)
+            {
+                elements.AppendLine(item);
+            }
+
+            Console.WriteLine(elements);
         }
     }
 }
