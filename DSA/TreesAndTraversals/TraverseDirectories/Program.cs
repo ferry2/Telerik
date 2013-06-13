1621a91c28045c98e6f8d466f27b4c60a26663d6
Vladislav Naydenov <ferry240@gmail.com>
2013-06-13 22:35:44 +0300

    Added new project.

---
+++ DSA/TreesAndTraversals/TraverseDirectories/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraverseDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ROOT = "C:\\Windows";
+
+            List<string> dirs = new List<string>();
+            List<string> exes = new List<string>();
+        }
+    }
+}
