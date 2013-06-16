51a40affd9bf05e710bd4479abcdc88daa097cba
Vladislav Naydenov <ferry240@gmail.com>
2013-06-16 23:39:48 +0300

    Solved first problem.

---
+++ DSA/Efficiency/Efficiency/Sorting.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wintellect.PowerCollections;
+
+namespace Efficiency
+{
+    class Sorting
+    {
+        static void Main(string[] args)
+        {
+            List<string[]> data = Read();
+
+            SortedDictionary<string, OrderedBag<Student>> cources = new SortedDictionary<string, OrderedBag<Student>>();
+
+            foreach (var student in data)
+            {
+                Student st = new Student(student[0], student[1]);
+                string key = student[2];
+
+                if (cources.ContainsKey(key))
+                {
+                    cources[key].Add(st);
+                }
+                else
+                {
+                    cources[key] = new OrderedBag<Student>
+                        {
+                            new Student(student[0], student[1])
+                        };
+                }
+            }
+
+            foreach (var student in cources)
+            {
+                Console.WriteLine(string.Format("{0} : {1}", student.Key, string.Join(", ", student.Value)));
+            }
+        }
+
+        public static List<string[]> Read()
+        {
+            List<string[]> students = new List<string[]>();
+
+            string path = "../../students.txt";
+            string[] lines = File.ReadAllLines(path);
+
+            foreach (var line in lines)
+            {
+                var student = line.Split(new char[] {'|', ' '}, StringSplitOptions.RemoveEmptyEntries);
+                students.Add(student);
+            }
+
+            return students;
+        }
+    }
+}
---
+++ DSA/Efficiency/Efficiency/Student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Efficiency
+{
+    public class Student : IComparable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Student(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Student name is: {0} {1}", this.FirstName, this.LastName);
+        }
+
+        /// <summary>
+        /// Compares the current instance with another object of the same type and returns an integer that indicates whether the current instance precedes, follows, or occurs in the same position in the sort order as the other object.
+        /// </summary>
+        /// <returns>
+        /// A value that indicates the relative order of the objects being compared. The return value has these meanings: Value Meaning Less than zero This instance precedes <paramref name="obj"/> in the sort order. Zero This instance occurs in the same position in the sort order as <paramref name="obj"/>. Greater than zero This instance follows <paramref name="obj"/> in the sort order. 
+        /// </returns>
+        /// <param name="obj">An object to compare with this instance. </param><exception cref="T:System.ArgumentException"><paramref name="obj"/> is not the same type as this instance. </exception>
+        public int CompareTo(object obj)
+        {
+            if (obj is Student)
+            {
+                var student = obj as Student;
+
+                if (this.LastName.CompareTo(student.LastName) > 0)
+                {
+                    return 1;
+                }
+                else if (this.LastName.CompareTo(student.LastName) < 0)
+                {
+                    return -1;
+                }
+                else
+                {
+                    if (this.FirstName.CompareTo(student.FirstName) > 0)
+                    {
+                        return 1;
+                    }
+                    else if (this.FirstName.CompareTo(this.FirstName) < 0)
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
